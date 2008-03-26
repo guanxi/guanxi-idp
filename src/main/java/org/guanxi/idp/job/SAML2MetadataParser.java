b9afa4aa09be2538d1609e441bb1e906a3c8c54f
Alistair Young <alistair@codebrane.com>
2008-03-26 11:10:29 +0000

    Added who for User-Agent

--- src/main/java/org/guanxi/idp/job/SAML2MetadataParser.java
+++ src/main/java/org/guanxi/idp/job/SAML2MetadataParser.java
@@ -11,6 +11,7 @@ import org.quartz.JobExecutionContext;
 import org.guanxi.idp.Bootstrap;
 import org.guanxi.xal.saml_2_0.metadata.EntityDescriptorType;
 import org.guanxi.xal.saml_2_0.metadata.EntitiesDescriptorDocument;
+import org.guanxi.xal.saml_2_0.metadata.SPSSODescriptorType;
 import org.guanxi.common.Utils;
 import org.guanxi.common.GuanxiException;
 import org.apache.xmlbeans.XmlException;
@@ -31,15 +32,19 @@ public class SAML2MetadataParser implements Job {
   }
 
   public void execute(JobExecutionContext context) throws JobExecutionException {
-    // and our custom config
+    // Get our custom config
     SAML2MetadataParserConfig config = (SAML2MetadataParserConfig)context.getJobDetail().getJobDataMap().get(Bootstrap.JOB_KEY_JOB_CONFIG);
 
     try {
-      EntitiesDescriptorDocument doc = Utils.parseSAML2Metadata(config.getMetadataURL());
-      EntityDescriptorType[] e = doc.getEntitiesDescriptor().getEntityDescriptorArray();
+      EntitiesDescriptorDocument doc = Utils.parseSAML2Metadata(config.getMetadataURL(), config.getWho());
+      EntityDescriptorType[] entityDescriptors = doc.getEntitiesDescriptor().getEntityDescriptorArray();
 
-      for (EntityDescriptorType t : e) {
-        config.getLog().info("Loading IdP metadata for : " + t.getEntityID());
+      for (EntityDescriptorType entityDescriptor : entityDescriptors) {
+        // Look for Service Providers
+        if (entityDescriptor.getSPSSODescriptorArray().length > 0) {
+          config.getLog().info("Loading SP metadata for : " + entityDescriptor.getEntityID());
+          config.getServletContext().setAttribute(entityDescriptor.getEntityID(), entityDescriptor);
+        }
       }
     }
     catch(GuanxiException ge) {
--- src/main/java/org/guanxi/idp/job/SAML2MetadataParserConfig.java
+++ src/main/java/org/guanxi/idp/job/SAML2MetadataParserConfig.java
@@ -10,7 +10,12 @@ import org.guanxi.common.job.SimpleGuanxiJobConfig;
 public class SAML2MetadataParserConfig extends SimpleGuanxiJobConfig {
   /** Where the get the SAML2 metadata */
   private String metadataURL = null;
+  /** The value of User-Agent to set */
+  private String who = null;
 
-  public String getMetadataURL() { return metadataURL; }
   public void setMetadataURL(String metadataURL) { this.metadataURL = metadataURL; }
+  public String getMetadataURL() { return metadataURL; }
+
+  public void setWho(String who) { this.who = who; }
+  public String getWho() { return who; }
 }
