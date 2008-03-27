e7df7f409406ef3c3596df0e1a6969b0f79acc74
Alistair Young <alistair@codebrane.com>
2008-03-27 10:52:29 +0000

    Implemented API methods

--- src/main/java/org/guanxi/idp/persistence/db/JDBCPersistenceEngine.java
+++ src/main/java/org/guanxi/idp/persistence/db/JDBCPersistenceEngine.java
@@ -17,16 +17,22 @@
 package org.guanxi.idp.persistence.db;
 
 import org.guanxi.idp.persistence.SimplePersistenceEngine;
+import org.guanxi.common.GuanxiPrincipal;
+import org.guanxi.common.GuanxiException;
 
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
+import java.util.Vector;
 
 /**
  * JDBC implementation of a PersistenceEngine
+ *
+ * DB schema:
+ * id userid attribute_name attribute_value relying_party
  */
 public class JDBCPersistenceEngine extends SimplePersistenceEngine {
+  private static final String WILDCARD = "__WILDCARD__";
+  
   // Injected
   private String framework = null;
   private String driver = null;
@@ -34,46 +40,99 @@ public class JDBCPersistenceEngine extends SimplePersistenceEngine {
   private String protocol = null;
   private String username = null;
   private String password = null;
+  private String databaseDirectory = null;
+  private String tableName = null;
+  private String fieldPrimaryKey = null;
+  private String fieldUserid = null;
+  private String fieldAttributeName = null;
+  private String fieldAttributeValue = null;
+  private String fieldRelyingParty = null;
 
   private Connection dbConnection = null;
-  private Properties props = null;
 
   public void init() {
     super.init();
 
-    loadDriver();
-
-    props = new Properties();
-    // Providing a user name and password is optional in the embedded and derbyclient frameworks
-    props.put("user", username);
-    props.put("password", password);
-
     try {
-      dbConnection = DriverManager.getConnection(protocol + databaseName + ";create=true", props);
+      connect();
+      if (!tableExists()) {
+        createTable();
+      }
     }
-    catch(SQLException sqle) {
-      log.error("Error connecting to database", sqle);
+    catch(GuanxiException ge) {
+      log.error("Error connecting to the database", ge);
     }
   }
 
   public void destroy() {
-    shutdownDatabase();
+    try {
+      disconnect();
+    }
+    catch(GuanxiException ge) {
+      log.error("Error disconnecting from the database", ge);
+    }
   }
   
-  public boolean attributeExists(String attributeName) {
-    return false;
+  public boolean attributeExists(GuanxiPrincipal principal, String attributeName) {
+    String[] columnNames = new String[] {fieldUserid, fieldAttributeName, fieldRelyingParty};
+    String[] columnValues = new String[] {principal.getName(), attributeName, principal.getRelyingPartyID()};
+
+    try {
+      String[] attributes = getField(tableName, columnNames, columnValues, fieldAttributeValue);
+      if (attributes.length > 0)
+        return true;
+      else
+        return false;
+    }
+    catch(GuanxiException ge) {
+      log.error("attributeExists error", ge);
+      return false;
+    }
   }
 
-  public String getAttributeValue(String attributeName) {
-    return null;
+  public String getAttributeValue(GuanxiPrincipal principal, String attributeName) {
+    String[] columnNames = new String[] {fieldUserid, fieldAttributeName, fieldRelyingParty};
+    String[] columnValues = new String[] {principal.getName(), attributeName, principal.getRelyingPartyID()};
+
+    try {
+      String[] attributes = getField(tableName, columnNames, columnValues, fieldAttributeValue);
+      if (attributes.length > 0) {
+        return attributes[0];
+      }
+      else
+        return null;
+    }
+    catch(GuanxiException ge) {
+      log.error("getAttributeValue error", ge);
+      return null;
+    }
   }
 
-  public boolean persistAttribute(String attributeName, String attributeValue) {
-    return false;
+  public boolean persistAttribute(GuanxiPrincipal principal, String attributeName, String attributeValue) {
+    try {
+      if (attributeExists(principal, attributeName)) {
+        update(principal.getName(), attributeName, attributeValue, principal.getRelyingPartyID());
+      }
+      else {
+        insert(principal.getName(), attributeName, attributeValue, principal.getRelyingPartyID());
+      }
+      return true;
+    }
+    catch(GuanxiException ge) {
+      log.error("persistAttribute error", ge);
+      return false;
+    }
   }
 
-  public boolean unpersistAttribute(String attributeName) {
-    return false;
+  public boolean unpersistAttribute(GuanxiPrincipal principal, String attributeName) {
+    try {
+      delete(principal.getName(), attributeName, principal.getRelyingPartyID());
+      return true;
+    }
+    catch(GuanxiException ge) {
+      log.error("unpersistAttribute error", ge);
+      return false;
+    }
   }
 
   /*
@@ -89,15 +148,19 @@ public class JDBCPersistenceEngine extends SimplePersistenceEngine {
    *  In an embedded environment, any static Derby system properties
    *  must be set before loading the driver to take effect.
    */
-  private void loadDriver() {
+  private void connect() throws GuanxiException {
     try {
       Class.forName(driver).newInstance();
+
+      Properties props = null;props = new Properties();
+      // Providing a user name and password is optional in the embedded and derbyclient frameworks
+      props.put("user", username);
+      props.put("password", password);
+
+      dbConnection = DriverManager.getConnection(protocol + databaseName + ";create=true", props);
     }
-    catch (ClassNotFoundException cnfe) {
-    }
-    catch (InstantiationException ie) {
-    }
-    catch (IllegalAccessException iae) {
+    catch (Exception e) {
+      throw new GuanxiException(e);
     }
   }
 
@@ -119,7 +182,7 @@ public class JDBCPersistenceEngine extends SimplePersistenceEngine {
    * Not shutting down when in a client environment, see method
    * Javadoc.
    */
-  private void shutdownDatabase() {
+  private void disconnect() throws GuanxiException {
     if (framework.equals("embedded")) {
       try {
         DriverManager.getConnection("jdbc:derby:;shutdown=true");
@@ -140,6 +203,153 @@ public class JDBCPersistenceEngine extends SimplePersistenceEngine {
         }
       }
     }
+
+    try {
+      if (dbConnection != null) {
+        if (!dbConnection.isClosed()) dbConnection.close();
+      }
+    }
+    catch (SQLException sqle) {
+      throw new GuanxiException(sqle);
+    }
+  }
+
+  private ResultSet query(String query) throws GuanxiException {
+    if (dbConnection == null) {
+      throw new GuanxiException("query cannot be performed : Not connected to the database");
+    }
+
+    try {
+      String sql = dbConnection.nativeSQL(query);
+      Statement statement = dbConnection.createStatement();
+      return statement.executeQuery(sql);
+    }
+    catch (SQLException sqle) {
+      throw new GuanxiException(sqle);
+    }
+  }
+
+  private void insert(String userid, String attributeName, String attributeValue, String relyingParty) throws GuanxiException {
+    String insertString = "insert into " + tableName + " (" + fieldUserid + ", " + fieldAttributeName + ", " + fieldAttributeValue + ", " + fieldRelyingParty + ") ";
+    insertString += " values(";
+    insertString += "'" + userid + "',";
+    insertString += "'" + attributeName + "',";
+    insertString += "'" + attributeValue + "',";
+    insertString += "'" + relyingParty + "'";
+    insertString += ")";
+
+    try {
+      Statement statement = dbConnection.createStatement();
+      statement.executeUpdate(insertString);
+    }
+    catch(SQLException sqle) {
+      throw new GuanxiException(sqle);
+    }
+  }
+
+  private void update(String userid, String attributeName, String attributeValue, String relyingParty) throws GuanxiException {
+    String updateString = "update " + tableName + "set ";
+    updateString += fieldAttributeValue + " = '" + attributeValue + "' ";
+    updateString += "where ";
+    updateString += fieldUserid + " = '" + userid + "' and";
+    updateString += fieldAttributeName + " = '" + attributeName + "' and";
+    updateString += fieldRelyingParty + " = '" + relyingParty + "'";
+
+    try {
+      Statement statement = dbConnection.createStatement();
+      statement.executeUpdate(updateString);
+    }
+    catch(SQLException sqle) {
+      throw new GuanxiException(sqle);
+    }
+  }
+
+  private void delete(String userid, String attributeName, String relyingParty) throws GuanxiException {
+    String deleteString = "delete from " + tableName + " where ";
+    deleteString += fieldUserid + " = '" + userid + "' and ";
+    deleteString += fieldAttributeName + " = '" + attributeName + "' and ";
+    deleteString += fieldRelyingParty + " = '" + relyingParty + "'";
+
+    try {
+      Statement statement = dbConnection.createStatement();
+      statement.executeUpdate(deleteString);
+    }
+    catch(SQLException sqle) {
+      throw new GuanxiException(sqle);
+    }
+  }
+
+  public String[] getField(String tableName, String[] columnNames, String[] columnValues, String fieldName) throws GuanxiException {
+    String[] fields = null;
+
+    String queryString = "select * from " + tableName + " where ";
+    for (int c=0; c < columnNames.length; c++) {
+      String op = null;
+      if (columnValues[c].equals(WILDCARD)) op = " like ";
+      else op = "=";
+
+      if (c == 0)
+        queryString += columnNames[c] + "" + op + "'" + columnValues[c] + "'";
+      else
+        queryString += " and " + columnNames[c] + "" + op + "'" + (columnValues[c].equals(WILDCARD) ? "%" : columnValues[c]) + "'";
+    }
+
+    ResultSet results = query(queryString);
+    if (results != null) {
+      Vector<String> buffer = new Vector<String>();
+      try {
+        while (results.next()) {
+          buffer.add(results.getString(fieldName));
+        }
+
+        results.close();
+
+        fields = new String[buffer.size()];
+        buffer.copyInto(fields);
+      }
+      catch(SQLException sqle) {
+        throw new GuanxiException(sqle);
+      }
+    }
+
+    return fields;
+  }
+
+  private boolean tableExists() throws GuanxiException {
+    try {
+      DatabaseMetaData meta = dbConnection.getMetaData();
+      ResultSet tables = meta.getTables(null, null, null, null);
+      
+      while(tables.next()) {
+        // Derby creates tables with uppercase names
+        if (tables.getString("TABLE_NAME").equals(tableName.toUpperCase())) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+    catch(SQLException sqle) {
+      throw new GuanxiException(sqle);
+    }
+  }
+
+  private void createTable() throws GuanxiException {
+    String createString = "create table " + tableName + " (";
+    createString += fieldPrimaryKey + " integer not null primary key generated always as identity (start with 1, increment by 1), ";
+    createString += fieldUserid + " varchar(255) not null, ";
+    createString += fieldAttributeName + " varchar(255) not null, ";
+    createString += fieldAttributeValue + " varchar(255) not null, ";
+    createString += fieldRelyingParty + " varchar(255) not null";
+    createString += ")";
+
+    try {
+      Statement statement = dbConnection.createStatement();
+      statement.executeUpdate(createString);
+    }
+    catch(SQLException sqle) {
+      throw new GuanxiException(sqle);
+    }
   }
 
   public void setDriver(String driver) { this.driver = driver; }
@@ -148,4 +358,11 @@ public class JDBCPersistenceEngine extends SimplePersistenceEngine {
   public void setUsername(String username) { this.username = username; }
   public void setPassword(String password) { this.password = password; }
   public void setFramework(String framework) { this.framework = framework; }
+  public void setDatabaseDirectory(String databaseDirectory) { this.databaseDirectory = databaseDirectory; }
+  public void setTableName(String tableName) { this.tableName = tableName; }
+  public void setFieldPrimaryKey(String fieldPrimaryKey) { this.fieldPrimaryKey = fieldPrimaryKey; }
+  public void setFieldUserid(String fieldUserid) { this.fieldUserid = fieldUserid; }
+  public void setFieldAttributeName(String fieldAttributeName) { this.fieldAttributeName = fieldAttributeName; }
+  public void setFieldAttributeValue(String fieldAttributeValue) { this.fieldAttributeValue = fieldAttributeValue; }
+  public void setFieldRelyingParty(String fieldRelyingParty) { this.fieldRelyingParty = fieldRelyingParty; }
 }
