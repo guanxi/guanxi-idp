6a8a7976a2376f0812ea171b16520862b249f013
Alistair Young <alistair@codebrane.com>
2008-04-11 09:43:56 +0000

    Removed init() method as the base class init() method wasn't called

--- src/test/java/org/guanxi/idp/attribute/AttributeTest.java
+++ src/test/java/org/guanxi/idp/attribute/AttributeTest.java
@@ -6,19 +6,13 @@
 package org.guanxi.idp.attribute;
 
 import org.guanxi.idp.IdPTest;
-import org.junit.BeforeClass;
 
 /**
  * Base class for attribute tests
  */
 public abstract class AttributeTest extends IdPTest {
-  protected static String[] attributorConfigFiles = null;
-
-  @BeforeClass
-  public static void init() {
-    attributorConfigFiles = new String[] {idpHome + "/WEB-INF/guanxi_idp/config/spring/farm/attributors.xml",
-                                          idpHome + "/WEB-INF/guanxi_idp/config/spring/farm/mapper.xml",
-                                          idpHome + "/WEB-INF/guanxi_idp/config/spring/farm/rules.xml",
-                                          idpHome + "/WEB-INF/guanxi_idp/config/spring/common.xml"};
-  }
+  protected static String[] attributorConfigFiles = new String[] {idpHome + "/WEB-INF/guanxi_idp/config/spring/farm/attributors.xml",
+                                                                  idpHome + "/WEB-INF/guanxi_idp/config/spring/farm/mapper.xml",
+                                                                  idpHome + "/WEB-INF/guanxi_idp/config/spring/farm/rules.xml",
+                                                                  idpHome + "/WEB-INF/guanxi_idp/config/spring/common.xml"};
 }
