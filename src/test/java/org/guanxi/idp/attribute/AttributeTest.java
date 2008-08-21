7f7a5a8e75bdf5373e6ac221f5b83e528a34863f
Alistair Young <alistair@codebrane.com>
2008-08-21 13:36:23 +0000

    Fixed issue with static initialisation order of class hierarchy

--- src/test/java/org/guanxi/idp/attribute/AttributeTest.java
+++ src/test/java/org/guanxi/idp/attribute/AttributeTest.java
@@ -6,13 +6,19 @@
 package org.guanxi.idp.attribute;
 
 import org.guanxi.idp.IdPTest;
+import org.junit.BeforeClass;
 
 /**
  * Base class for attribute tests
  */
 public abstract class AttributeTest extends IdPTest {
-  protected static String[] attributorConfigFiles = new String[] {idpHome + "/WEB-INF/guanxi_idp/config/spring/farm/attributors.xml",
-                                                                  idpHome + "/WEB-INF/guanxi_idp/config/spring/farm/mapper.xml",
-                                                                  idpHome + "/WEB-INF/guanxi_idp/config/spring/farm/rules.xml",
-                                                                  idpHome + "/WEB-INF/guanxi_idp/config/spring/farm/persistence.xml"};
+  protected static String[] attributorConfigFiles = null;
+
+  @BeforeClass
+  public static void initAttributeTest() {
+    attributorConfigFiles = new String[] {idpHome + "/WEB-INF/guanxi_idp/config/spring/farm/attributors.xml",
+                                          idpHome + "/WEB-INF/guanxi_idp/config/spring/farm/mapper.xml",
+                                          idpHome + "/WEB-INF/guanxi_idp/config/spring/farm/rules.xml",
+                                          idpHome + "/WEB-INF/guanxi_idp/config/spring/farm/persistence.xml"};
+  }
 }
