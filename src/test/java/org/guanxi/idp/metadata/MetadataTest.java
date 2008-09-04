903c3bc8ff969e586f6d7356622a507eba9d8273
Alistair Young <alistair@codebrane.com>
2008-09-04 21:26:48 +0000

    Updated to test new metadata framework

--- src/test/java/org/guanxi/idp/metadata/MetadataTest.java
+++ src/test/java/org/guanxi/idp/metadata/MetadataTest.java
@@ -9,7 +9,11 @@ import org.guanxi.idp.IdPTest;
 import org.junit.BeforeClass;
 
 public abstract class MetadataTest extends IdPTest {
+  protected static String[] metadataConfigFiles = null;
+  
   @BeforeClass
   public static void initMetadataTest() {
+    metadataConfigFiles = new String[] {idpHome + "/WEB-INF/guanxi_idp/config/spring/application/jobs/ukFederationMetadataParser.xml",
+                                        idpHome + "/WEB-INF/guanxi_idp/config/spring/application/entity.xml"};
   }
 }
