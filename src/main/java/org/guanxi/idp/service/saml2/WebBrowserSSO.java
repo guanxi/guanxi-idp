74e55646ccccfce36e57318430edea52673bffb6
Alistair Young <alistair@codebrane.com>
2009-12-10 15:25:59 +0000

    Now delegates all verification, authentication and error generation to its auth handler

--- src/main/java/org/guanxi/idp/service/saml2/WebBrowserSSO.java
+++ src/main/java/org/guanxi/idp/service/saml2/WebBrowserSSO.java
@@ -19,9 +19,10 @@ package org.guanxi.idp.service.saml2;
 import org.springframework.web.servlet.mvc.AbstractController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.context.ServletContextAware;
-import org.guanxi.common.definitions.Shibboleth;
+import org.springframework.context.MessageSource;
 import org.guanxi.xal.saml_2_0.protocol.ResponseDocument;
 import org.guanxi.xal.saml_2_0.protocol.ResponseType;
+import org.apache.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,29 +30,63 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * SAML2 Web Browser SSO Single Sign-On Service.
  * This is the authentication request protocol endpoint at the identity provider to which the
- * <AuthnRequest> message (or artifact representing it) is delivered by the user agent. 
+ * <AuthnRequest> message (or artifact representing it) is delivered by the user agent.
+ *
+ * @author alistair
  */
 public class WebBrowserSSO extends AbstractController implements ServletContextAware {
-  private String responseView = null;
+  /** Our logger */
+  private static final Logger logger = Logger.getLogger(WebBrowserSSO.class.getName());
+  /** The localised messages to use */
+  private MessageSource messages = null;
+  /** The JSP to use to POST the response to the SP */
+  private String httpPOSTView = null;
+  /** The JSP to display if an error occurs */
   private String errorView = null;
+  /** The request attribute that holds the error message for the error view */
+  private String errorViewDisplayVar = null;
 
   public void init() {}
 
+  /**
+   * Handles processing of an AuthnRequest message. If the request attribute:
+   * wbsso-handler-error-message
+   * is present then we must display the error text it contains and go no further.
+   * 
+   * @param request
+   * @param response
+   * @return
+   * @throws Exception
+   */
   public ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
     ModelAndView mAndV = new ModelAndView();
+
+    // Display an error message if it exists and go no further
+    if (request.getAttribute("wbsso-handler-error-message") != null) {
+      logger.error("Displaying auth handler error");
+      mAndV.setViewName(errorView);
+      mAndV.getModel().put(errorViewDisplayVar, request.getAttribute("wbsso-handler-error-message"));
+      return mAndV;
+    }
 
     ResponseDocument responseDoc = ResponseDocument.Factory.newInstance();
     ResponseType wbssoResponse = responseDoc.addNewResponse();
 
     // HTTP POST or HTTP Artifact
+
+    String b64SAMLResponse = null;
       
     // Send the Response to the SP
-    mAndV.setViewName(responseView);
-    mAndV.getModel().put(Shibboleth.SHIRE, request.getParameter(Shibboleth.SHIRE));
+    request.setAttribute("SAMLResponse", b64SAMLResponse);
+    request.setAttribute("RelayState", request.getParameter("RelayState"));
+    mAndV.setViewName(httpPOSTView);
+    mAndV.getModel().put("wbsso_acs_endpoint", request.getAttribute("acsURL"));
     return mAndV;
   }
 
   // Setters
-  public void setResponseView(String responseView) { this.responseView = responseView; }
+  public void setMessages(MessageSource messages) { this.messages = messages; }
+  public void setHttpPOSTView(String httpPOSTView) { this.httpPOSTView = httpPOSTView; }
   public void setErrorView(String errorView) { this.errorView = errorView; }
+  public void setErrorViewDisplayVar(String errorViewDisplayVar) { this.errorViewDisplayVar = errorViewDisplayVar; }
 }
