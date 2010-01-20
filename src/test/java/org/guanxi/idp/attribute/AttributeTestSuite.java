c13d7e17779ba6431524d51f57ec1bba81bf00e5
Alistair Young <alistair@codebrane.com>
2010-01-20 15:05:46 +0000

    Added FlatFileAttributeSAML2Test

--- src/test/java/org/guanxi/idp/attribute/AttributeTestSuite.java
+++ src/test/java/org/guanxi/idp/attribute/AttributeTestSuite.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { FlatFileAttributeTest.class } )
+@Suite.SuiteClasses( { FlatFileAttributeTest.class,
+                       FlatFileAttributeSAML2Test.class} )
 public class AttributeTestSuite {
 }
