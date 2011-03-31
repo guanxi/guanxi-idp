0806a5fbb88d5a3b7e7ca6cca307e184334895d8
Alistair Young <alistair@codebrane.com>
2011-03-31 14:30:14 +0100

    Updated to use resources in src

--- src/test/java/org/guanxi/idp/Paths.java
+++ src/test/java/org/guanxi/idp/Paths.java
@@ -1,40 +1,44 @@
 package org.guanxi.idp;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.io.File;
-import java.io.IOException;
+
+import static org.junit.Assert.fail;
 
 /**
  * Conveneience class for working out test file paths
  */
 public class Paths {
-  private static final String ROOT = "/contextroot/WEB-INF/guanxi_idp/config";
+  private static String ROOT = null;
   private static HashMap<String, String> paths = null;
 
   static {
-    paths = new HashMap<String, String>();
     try {
-      // Spring needs file:// prepended...
-      paths.put("servlet.context.home", "file:///" + new File(Paths.class.getResource("/contextroot").getPath()).getCanonicalPath());
-      paths.put("attributors.xml", "file:///" + new File(Paths.class.getResource(ROOT + "/spring/farm/attributors.xml").getPath()).getCanonicalPath());
-      paths.put("arp-engine.xml", "file:///" + new File(Paths.class.getResource(ROOT + "/spring/farm/arp.xml").getPath()).getCanonicalPath());
-      paths.put("mapper.xml", "file:///" + new File(Paths.class.getResource(ROOT + "/spring/farm/mapper.xml").getPath()).getCanonicalPath());
-      paths.put("rules.xml", "file:///" + new File(Paths.class.getResource(ROOT + "/spring/farm/rules.xml").getPath()).getCanonicalPath());
-      paths.put("persistence.xml", "file:///" + new File(Paths.class.getResource(ROOT + "/spring/farm/persistence.xml").getPath()).getCanonicalPath());
-      paths.put("var-engine.xml", "file:///" + new File(Paths.class.getResource(ROOT + "/spring/farm/var-engine.xml").getPath()).getCanonicalPath());
-      paths.put("ukFederationMetadataParser.xml", "file:///" + new File(Paths.class.getResource(ROOT + "/spring/application/jobs/ukFederationMetadataParser.xml").getPath()).getCanonicalPath());
-      paths.put("entity.xml", "file:///" + new File(Paths.class.getResource(ROOT + "/spring/application/entity.xml").getPath()).getCanonicalPath());
-      paths.put("persistence.xml", "file:///" + new File(Paths.class.getResource(ROOT + "/spring/farm/persistence.xml").getPath()).getCanonicalPath());
-      paths.put("aa-service.xml", "file:///" + new File(Paths.class.getResource(ROOT + "/spring/services/shibboleth/aa-service.xml").getPath()).getCanonicalPath());
-
-      // ...which breaks XMLBeans!
-      paths.put("vars.xml", new File(Paths.class.getResource(ROOT + "/shared/vars.xml").getPath()).getCanonicalPath());
-      paths.put("arp.xml", new File(Paths.class.getResource(ROOT + "/shared/arp.xml").getPath()).getCanonicalPath());
-      paths.put("flatfile.xml", new File(Paths.class.getResource(ROOT + "/shared/flatfile.xml").getPath()).getCanonicalPath());
-      paths.put("saml2map.xml", new File(Paths.class.getResource(ROOT + "/shared/saml2map.xml").getPath()).getCanonicalPath());
+      ROOT = new File(".").getCanonicalPath() + "/src/main/webapp";
     }
     catch(IOException ioe) {
+      fail(ioe.getMessage());
     }
+    paths = new HashMap<String, String>();
+    // Spring needs file:// prepended...
+    paths.put("servlet.context.home", "file:///" + ROOT);
+    paths.put("attributors.xml", "file:///" + ROOT + "/WEB-INF/guanxi_idp/config/spring/farm/attributors.xml");
+    paths.put("arp-engine.xml", "file:///" + ROOT + "/WEB-INF/guanxi_idp/config/spring/farm/arp.xml");
+    paths.put("mapper.xml", "file:///" + ROOT + "/WEB-INF/guanxi_idp/config/spring/farm/mapper.xml");
+    paths.put("rules.xml", "file:///" + ROOT + "/WEB-INF/guanxi_idp/config/spring/farm/rules.xml");
+    paths.put("persistence.xml", "file:///" + ROOT + "/WEB-INF/guanxi_idp/config/spring/farm/persistence.xml");
+    paths.put("var-engine.xml", "file:///" + ROOT + "/WEB-INF/guanxi_idp/config/spring/farm/var-engine.xml");
+    paths.put("ukFederationMetadataParser.xml", "file:///" + ROOT + "/WEB-INF/guanxi_idp/config/spring/application/jobs/ukFederationMetadataParser.xml");
+    paths.put("entity.xml", "file:///" + ROOT + "/WEB-INF/guanxi_idp/config/spring/application/entity.xml");
+    paths.put("persistence.xml", "file:///" + ROOT + "/WEB-INF/guanxi_idp/config/spring/farm/persistence.xml");
+    paths.put("aa-service.xml", "file:///" + ROOT + "/WEB-INF/guanxi_idp/config/spring/services/shibboleth/aa-service.xml");
+
+    // ...which breaks XMLBeans!
+    paths.put("vars.xml", ROOT + "/WEB-INF/guanxi_idp/config/shared/vars.xml");
+    paths.put("arp.xml", ROOT + "/WEB-INF/guanxi_idp/config/shared/arp.xml");
+    paths.put("flatfile.xml", ROOT + "/WEB-INF/guanxi_idp/config/shared/flatfile.xml");
+    paths.put("saml2map.xml", ROOT + "/WEB-INF/guanxi_idp/config/shared/saml2map.xml");
   }
 
   public static String path(String fileName) {
